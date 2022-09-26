3c4f0fa86ce374ae6245685adac11af74080721b
nico bazzoni <nicobazzoni@gmail.com>
2022-09-26 18:50:17 -0400

    top chart card created

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,7 +14,13 @@ import { createApi , fetchBaseQuery} from '@reduxjs/toolkit/query/react';
       },
      }),
         endpoints: (builder) => ({
-          getTopCharts: builder.query({ query: () => '/charts/world' }),  
+          getTopCharts: builder.query({ query: () => '/charts/world' }), 
+          getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+          getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+          getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+          getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+          getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+          getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }), 
         }),
 
 
@@ -22,5 +28,11 @@ import { createApi , fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
     export const {
     useGetTopChartsQuery, 
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
     } = shazamCoreApi
     
